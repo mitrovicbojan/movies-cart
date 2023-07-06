acf32aafbfd05f7a4d8ed2cf465e356375a1d6c6
unknown <mitrovicbojan87@gmail.com>
2023-07-06 20:58:28 +0300

    remove

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ function addToCart(elements) {
   let title = mainItem.querySelector("h3").innerText;
   let cartItems = document.querySelector(".cart-items");
   price = Number(price.substring(1));
-  console.log(price);
+
   totalPrice += price;
-  cartItems.innerHTML += `Movie: ${title}, Price:${price} <button>Remove</button>`;
+  cartItems.innerHTML += `Movie: ${title}, Price:${price} <button class="remove-item" onclick="removeFromCart(this)">Remove</button> <br>`;
   document.querySelector(".total").innerText = `Total price: $${totalPrice}`;
   elements.setAttribute("disabled", "true");
+}
+
+function removeFromCart(elements) {
+  console.log("remove");
 }
